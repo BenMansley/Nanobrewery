eb86696dcfd48ddc23ca3c1b70e65ea4f282b870
Ben Mansley <ben@eduroam-221-40.uea.ac.uk>
2018-01-18 18:02:19 +0000

    Would be nice if we actually returned something

--- routes/customizer.js
+++ routes/customizer.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.get('/variables', function(req, res, next) {
   req.app.settings.conn.query('SELECT * FROM Variables', (error, results, fields) => {
     if (error) return res.status(500).json(error);
-    console.log(res.json(results));
+    res.status(200).json(results);
   });
 });
 
