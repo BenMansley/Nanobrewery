89f967ca3d59263d6ed19d36b9b18b49b2902ca2
Ben Mansley <ben@eduroam-222-179.uea.ac.uk>
2018-01-27 16:01:50 +0000

    Work on splash page

--- client/src/admin/user/welcome.component.js
+++ client/src/admin/user/welcome.component.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-const Welcome = ({user}) =>{
+const Welcome = ({user}) => {
   return (
     <div className="page-content">
-      <h1 className="page-title">Welcome to the Nanobrewing Club, {user.name}!</h1>
+      <h1 className="page-title">Welcome to the Nanobrewing Club, {user.name.split(/\s+/)[0]}!</h1>
       <p>You're all ready to get set up.</p>
+      <h2>First Steps</h2>
+      <div className="welcome-steps">
+        <Link to="/shop/hardware" className="card step hardware">
+          <div className="step-flex">
+            <div className="step-image"><span className="step-complete material-icons">done</span></div>
+            <span className="step-title">Get the Hardware</span>
+          </div>
+        </Link>
+        <span className="step-next material-icons">arrow_forward</span>
+        <Link to="/admin/customizer" className="card step make">
+          <div className="step-flex">
+            <div className="step-image"></div>
+            <span className="step-title">Make Your First Beer</span>
+          </div>
+        </Link>
+        <span className="step-next material-icons">arrow_forward</span>
+        <Link to="/admin/branding" className="card step brand">
+          <div className="step-flex">
+            <div className="step-image"><span className="step-incomplete material-icons">clear</span></div>
+            <span className="step-title">Brand Your Beer</span>
+          </div>
+        </Link>
+      </div>
     </div>
   );
 }
@@ -16,7 +40,7 @@ Welcome.propTypes = {
     id: PropTypes.number,
     email: PropTypes.string,
     name: PropTypes.string,
-    companyName: PropTypes.string    
+    companyName: PropTypes.string
   }).isRequired
 };
 
