3eaefb473938e94e5db1562e57e59cef6596f6d9
Ben Mansley <ben@eduroam-223-28.uea.ac.uk>
2018-02-01 18:57:16 +0000

    Moved admin routing into separate component

---
+++ client/src/admin/admin.component.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom'; 
+import Account from './user/account.component';
+import Welcome from './user/welcome.component';
+import Customizer from './customizer/customizer.component';
+import Dashboard from './customizer/dashboard.component';
+import Basket from './shop/basket/basket.component';
+
+const Admin = () => {
+  return (
+    <Switch>
+      <Route exact path='/admin/user/account' component={Account}/>
+      <Route exact path='/admin/user/welcome' component={Welcome}/>
+      <Route exact path='/admin/customizer' component={Customizer}/>          
+      <Route exact path='/admin/dashboard' component={Dashboard}/>
+      <Route exact path='/admin/basket' component={Basket}/>
+    </Switch>
+  );
+}
+
+export default Admin;
