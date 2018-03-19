e7d53e7dbaa8dbc679e60fff77d67c6eb615f074
Ben Mansley <ben@Bens-MBP-4.home>
2018-03-19 14:34:40 +0000

    Refactoring

--- routes/customizer.js
+++ routes/customizer.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const conn = require('../app').get('conn');
+
 router.get('/variables', (req, res, next) => {
-  req.app.settings.conn.query('SELECT * FROM Variables', (error, results, fields) => {
+  conn.query('SELECT * FROM Variables', (error, results, fields) => {
     if (error) return res.status(500).json(error);
     return res.status(200).json(results);
   });
@@ -12,7 +14,7 @@ router.put('/variables', (req, res, next) => {
   const { id, name, min, max, step, defaultVal, suffix } = req.body;
   const queryString = 
     `UPDATE Variables SET name='${name}', min=${min}, max=${max}, step=${step}, defaultVal=${defaultVal}, suffix='${suffix}' WHERE id=${id}`;
-  req.app.settings.conn.query(queryString, (error, results, fields) => {
+  conn.query(queryString, (error, results, fields) => {
     if (error) return res.status(400).json(error.sqlMessage);
     return res.status(200).json(results);
   });
--- routes/users.js
+++ routes/users.js
@@ -2,26 +2,31 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 
+const conn = require('../app').get('conn');
+
 /* GET users listing. */
 router.get('/all', (req, res, next) => {
-  req.app.settings.conn.query('SELECT * FROM Users', (error, results, fields) => {
-    if (error) throw error;
+  conn.query('SELECT * FROM Users', (error, results, fields) => {
+    if (error) return res.status(500).json(error);
     res.json(results);
   });
 });
 
 router.get('/email/:email', (req, res, next) => {
-  req.app.settings.conn.query(`SELECT * FROM Users WHERE email='${req.params.email}'`, (error, results, fields) => {
-    if (error) throw error;
+  conn.query(`SELECT * FROM Users WHERE email='${req.params.email}'`, (error, results, fields) => {
+    if (error) return res.status(500).json(error);
     res.json(results);
   });
 });
 
 router.post('/from-token', (req, res, next) => {
   const { id, token } = req.body;
-
-  req.app.settings.conn.query(`SELECT * FROM Tokens WHERE userid=${id} AND token='${token}'`, (error, results, fields) => {
-    if (error) return res.status(500).json(error);
+  const error = 'Error retrieving login data';
+  conn.query(`SELECT * FROM Tokens WHERE userid=${id} AND token='${token}'`, (err, results, fields) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json(error);
+    }
     if (results.length === 0) {
       return res.status(400).json('Invalid Token');
     }
@@ -29,8 +34,11 @@ router.post('/from-token', (req, res, next) => {
     if (Date.parse(results[0].expiry) < new Date()) {
       return res.status(400).json('Expired Token');
     }
-    req.app.settings.conn.query(`SELECT id, email, name, companyName FROM Users WHERE id=${id}`, (error, results, fields) => {
-      if (error) return res.status(500).json(error);
+    conn.query(`SELECT id, email, name, companyName FROM Users WHERE id=${id}`, (err, results, fields) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json(error);
+      }
       return res.status(200).json(results[0]);
     });
   });
@@ -39,12 +47,12 @@ router.post('/from-token', (req, res, next) => {
 router.post('/', (req, res, next) => {
   const { email, password } = req.body;
   
-  req.app.settings.conn.query(`SELECT * FROM Users WHERE email='${email}'`, (error, results, fields) => {
-    if (error) return res.status(500).json(error);
+  conn.query(`SELECT * FROM Users WHERE email='${email}'`, (error, results, fields) => {
+    if (error) return res.status(500).json('Error logging in');
     bcrypt.compare(password, results[0].password, (err, match) => {
       if (!match) return res.status(400).json('Invalid Email or Password');
       delete results[0].password;
-      results['sessiontoken'] = generateToken(results[0].id, req.app.settings.conn);
+      results['sessiontoken'] = generateToken(results[0].id, conn);
       return res.json(results[0]);
     });
   });
@@ -59,14 +67,14 @@ router.post('/new', (req, res, next) => {
 
     let queryString = `SELECT * FROM Users WHERE email='${email}'`;
   
-    req.app.settings.conn.query(queryString, (error, results, fields) => {
+    conn.query(queryString, (error, results, fields) => {
       if (error) return res.status(500).json(error);
       if (results.length !== 0) return res.status(400).json('A user with that email already exists');
       queryString = `INSERT INTO Users (email, password, name, companyName)
                       VALUES ('${email}','${hash}','${name}','${companyName}')`;
   
-      req.app.settings.conn.query(queryString, (error, results, fields) => {
-        if (error) throw error;
+      conn.query(queryString, (error, results, fields) => {
+        if (error) return res.status(500).json(error);
         res.json({ id: results.insertId, name, email, companyName });
       });
     });
@@ -76,9 +84,8 @@ router.post('/new', (req, res, next) => {
 router.put('/edit', (req, res, next) => {
   const { id, email, name, companyName } = req.body;
   const queryString = `UPDATE Users SET email='${email}', name='${name}', companyName='${companyName}' WHERE id=${id}`;
-  req.app.settings.conn.query(queryString, (error, results, fields) => {
-    console.log(error);
-    if (error) return res.status(400).json(error.sqlMessage);
+  conn.query(queryString, (error, results, fields) => {
+    if (error) return res.status(500).json(error);
     return res.status(200).json(req.body);
   });
 });
