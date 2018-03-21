db208626b6b33f38356caffa93b4d4f44f55626f
Ben Mansley <ben@Bens-MBP-4.lan>
2018-03-21 18:51:09 +0000

    Cleaned up app, changed start script

--- app.js
+++ app.js
@@ -2,26 +2,18 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 const html = require('html');
-const helmet = require('helmet');
-const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+
 const mysql = require('mysql');
 
+const app = module.exports = express();
 const index = require('./routes/index');
 const users = require('./routes/users');
 const customizer = require('./routes/customizer');
 
-const app = express();
-
-app.use(helmet());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, './client/build')));
-
-app.use('/api/users', users);
-app.use('/api/customizer', customizer);
 
 const PORT = process.env.PORT || 3001;
 
@@ -35,24 +27,18 @@ const conn = mysql.createPool({
 
 app.set('conn', conn);
 
+app.use('/api/users', users);
+app.use('/api/customizer', customizer);
+
+app.use(express.static(path.join(__dirname, 'client/build')))
+
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
   res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
 });
 
-// error handler
-app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.sendFile(__dirname + '/error.view.html');
+app.listen(PORT, () => {
+  console.log(`Nanobrewery listening on port: ${PORT}`);
 });
-
-// app.listen(PORT, () => {
-//   console.log(`Nanobrewery listening on port: ${PORT}`);
-// })
 
 module.exports = app;
