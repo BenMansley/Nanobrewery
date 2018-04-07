796d9c41b643a7c1b4dde8523898469b17d00d2e
Ben Kolya Mansley <ben@kolyanet.com>
2018-04-07 14:24:23 +0100

    Minor fixes

--- client/src/components/customization-table/customization-table.component.js
+++ client/src/components/customization-table/customization-table.component.js
@@ -5,18 +5,22 @@ import { Link } from "react-router-dom";
 const CustomizationTable = ({ customizations }) => {
   let customizationLines = [];
   if (customizations.length > 0) {
-    customizationLines = customizations.map(c => 
+    customizationLines = customizations.map(c =>
       <div key={c.id} className="customization-line">
         <h2>{c.name}</h2>
         <div className="customization-actions">
-          <Link className="button" to={{ pathname: "/admin/customizer", search:`?id=${c.id}` }}>Edit</Link>          
+          <Link className="button" to={{ pathname: "/admin/customizer", search:`?id=${c.id}` }}>Edit</Link>
           <Link className="button" to={{ pathname: "/admin/branding", search:`?id=${c.id}` }}>Brand</Link>
         </div>
       </div>
     );
   }
-  
-  return <div className="customization-table card">{customizationLines}</div>
-}
 
-export default CustomizationTable
+  return <div className="customization-table card">{customizationLines}</div>;
+};
+
+CustomizationTable.propTypes = {
+  customizations: PropTypes.array
+};
+
+export default CustomizationTable;
